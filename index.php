d412b7b46b159dc1dba7507d4910e135b1952ab1
Aïmen <aimen2nice@gmail.com>
2024-10-21 10:16:48 +0200

    ok

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Runtrack|Jour01</title>
+    <SCRIPT LANGUAGE="JavaScript">
+             alert('Hello Javascript!');
+
+    </script>
+</head>
+<body>
+</body>
+</html>
