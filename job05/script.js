56e4ec72e44b85e4cda773c40da5fdaa367daf7f
Aïmen <aimen2nice@gmail.com>
2024-10-21 11:20:28 +0200

    job05 ok

---
+++ job05/script.js
@@ -0,0 +1,8 @@
+function aﬃcherjourssemaines()
+{
+        let jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+        jourssemaines.forEach((item) => console.log(item));
+
+}
+
+aﬃcherjourssemaines();
